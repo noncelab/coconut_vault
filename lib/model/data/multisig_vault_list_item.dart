a0c263eb0a63abbf4f018d671aba5fa464c917f7
pio <pio@noncelab.com>
2024-11-29 11:28:09 +0900

    issue #57 fix(vault-menu): 싱글 시그 볼트 메뉴 수정(내보내기 -> 지갑정보내보내기, 다중 서명 키로 사용하기)

--- lib/model/data/multisig_vault_list_item.dart
+++ lib/model/data/multisig_vault_list_item.dart
@@ -39,8 +39,9 @@ class MultisigVaultListItem extends VaultListItemBase {
     coconutVault = MultisignatureVault.fromCoordinatorBsms(coordinatorBsms!);
     for (int i = 0; i < signers.length; i++) {
       if (signers[i].keyStore.hasSeed) {
-        (coconutVault as MultisignatureVault)
-            .bindSeedToKeyStore(signers[i].keyStore.seed, i);
+        (coconutVault as MultisignatureVault).bindSeedToKeyStore(
+          signers[i].keyStore.seed,
+        );
       }
     }
 
