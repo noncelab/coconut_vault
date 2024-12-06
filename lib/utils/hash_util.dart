494150f51df76d27a3a8c9a310aed38b3a663016
ella <eunji@noncelab.com>
2024-12-06 10:34:26 +0900

    feat(crypto): pubspec.yaml에 패키지 추가 명시

--- lib/utils/hash_util.dart
+++ lib/utils/hash_util.dart
@@ -4,5 +4,6 @@ import 'package:crypto/crypto.dart';
 String hashString(String input) {
   final bytes = utf8.encode(input);
   final digest = sha256.convert(bytes);
+
   return digest.toString();
 }
---
+++ test/utils/hash_utils_test.dart
@@ -0,0 +1,42 @@
+import 'package:coconut_vault/utils/hash_util.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('hashString', () {
+    test('hashes a string correctly', () {
+      final input = "hello";
+      final expectedHash =
+          "5d41402abc4b2a76b9719d911017c592"; // "hello"의 MD5 해시 값 (예시)
+
+      // SHA-256 해시 결과
+      final sha256ExpectedHash =
+          "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
+
+      print('result: ${hashString(input)}');
+      expect(hashString(input), equals(sha256ExpectedHash));
+    });
+
+    test('produces different hashes for different inputs', () {
+      final hash1 = hashString("hello");
+      final hash2 = hashString("world");
+
+      expect(hash1, isNot(equals(hash2)));
+    });
+
+    test('produces consistent hash for the same input', () {
+      final input = "testInput";
+      final hash1 = hashString(input);
+      final hash2 = hashString(input);
+
+      expect(hash1, equals(hash2));
+    });
+
+    test('handles empty input', () {
+      final hash = hashString("");
+      final expectedHash =
+          "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"; // SHA-256 해시 값
+
+      expect(hash, equals(expectedHash));
+    });
+  });
+}
