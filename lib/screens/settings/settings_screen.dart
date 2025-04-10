a0ec5163b0f02fbd04edaad2387059959ba08f79
pio <pio@noncelab.com>
2025-04-10 12:23:31 +0900

    fix(setting-menu-update): 볼트리스트 없을 때 '업데이트 준비'요소 보이지 않게 처리, 위젯 세분화 작업
    #105

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:coconut_vault/constants/app_routes.dart';
 import 'package:coconut_vault/enums/pin_check_context_enum.dart';
 import 'package:coconut_vault/localization/strings.g.dart';
 import 'package:coconut_vault/providers/auth_provider.dart';
+import 'package:coconut_vault/providers/wallet_provider.dart';
 import 'package:coconut_vault/utils/logger.dart';
 import 'package:coconut_vault/widgets/custom_loading_overlay.dart';
 import 'package:flutter/cupertino.dart';
@@ -57,8 +58,11 @@ class _SettingsScreenState extends State<SettingsScreen> {
               const SizedBox(
                 height: 40,
               ),
-              /*_informationPart(),
-              const SizedBox(height: 32),*/
+              _updatePart(context),
+              const SizedBox(
+                height: 40,
+              ),
+              _advancedUserPart(context),
             ],
           ),
         ),
@@ -71,113 +75,139 @@ class _SettingsScreenState extends State<SettingsScreen> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Container(
-          padding: const EdgeInsets.symmetric(vertical: 12),
+          padding: const EdgeInsets.only(bottom: 16),
           child: Text(t.security, style: CoconutTypography.body1_16_Bold),
         ),
-        Consumer<AuthProvider>(builder: (context, provider, child) {
-          return ButtonGroup(buttons: [
-            if (provider.isPinSet) ...{
-              if (provider.canCheckBiometrics)
+        Consumer<AuthProvider>(
+          builder: (context, provider, child) {
+            return ButtonGroup(buttons: [
+              if (provider.isPinSet) ...{
+                if (provider.canCheckBiometrics)
+                  SingleButton(
+                    title: t.settings_screen.use_biometric,
+                    rightElement: CupertinoSwitch(
+                      value: provider.hasBiometricsPermission
+                          ? provider.isBiometricEnabled
+                          : false,
+                      activeColor: MyColors.primary,
+                      onChanged: (isOn) async {
+                        if (isOn &&
+                            await provider.authenticateWithBiometrics(context,
+                                isSaved: true)) {
+                          Logger.log('Biometric authentication success');
+                          provider.saveIsBiometricEnabled(true);
+                        } else {
+                          Logger.log('Biometric authentication fail');
+                          provider.saveIsBiometricEnabled(false);
+                        }
+                      },
+                    ),
+                  ),
                 SingleButton(
-                  title: t.settings_screen.use_biometric,
+                  title: t.settings_screen.change_password,
+                  onPressed: () async {
+                    MyBottomSheet.showBottomSheet_90(
+                      context: context,
+                      child: const LoaderOverlay(
+                        child: PinCheckScreen(
+                          pinCheckContext: PinCheckContextEnum.change,
+                        ),
+                      ),
+                    );
+                  },
+                )
+              } else ...{
+                SingleButton(
+                  title: t.settings_screen.set_password,
                   rightElement: CupertinoSwitch(
-                    value: provider.hasBiometricsPermission
-                        ? provider.isBiometricEnabled
-                        : false,
+                    value: provider.isPinSet,
                     activeColor: MyColors.primary,
-                    onChanged: (isOn) async {
-                      if (isOn &&
-                          await provider.authenticateWithBiometrics(context,
-                              isSaved: true)) {
-                        Logger.log('Biometric authentication success');
-                        provider.saveIsBiometricEnabled(true);
-                      } else {
-                        Logger.log('Biometric authentication fail');
-                        provider.saveIsBiometricEnabled(false);
-                      }
+                    onChanged: (value) {
+                      MyBottomSheet.showBottomSheet_90(
+                        context: context,
+                        child: const PinSettingScreen(),
+                      );
                     },
                   ),
                 ),
-              SingleButton(
-                title: t.settings_screen.change_password,
-                onPressed: () async {
-                  MyBottomSheet.showBottomSheet_90(
-                    context: context,
-                    child: const LoaderOverlay(
-                      child: PinCheckScreen(
-                        pinCheckContext: PinCheckContextEnum.change,
-                      ),
-                    ),
-                  );
-                },
-              )
-            } else ...{
-              SingleButton(
-                title: t.settings_screen.set_password,
-                rightElement: CupertinoSwitch(
-                  value: provider.isPinSet,
-                  activeColor: MyColors.primary,
-                  onChanged: (value) {
-                    MyBottomSheet.showBottomSheet_90(
-                      context: context,
-                      child: const PinSettingScreen(),
-                    );
-                  },
-                ),
-              ),
-            }
-          ]);
-        }),
+              }
+            ]);
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _updatePart(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
         Container(
-          padding: const EdgeInsets.only(top: 45, bottom: 16),
+          padding: const EdgeInsets.only(bottom: 16),
           child: Text(
             t.settings_screen.update,
             style: CoconutTypography.body1_16_Bold,
           ),
         ),
-        ButtonGroup(
-          buttons: [
-            SingleButton(
-              title: t.settings_screen.prepare_update,
-              onPressed: () async {
-                MyBottomSheet.showBottomSheet_90(
-                  context: context,
-                  child: CustomLoadingOverlay(
-                    child: PinCheckScreen(
-                      pinCheckContext: PinCheckContextEnum.sensitiveAction,
-                      isDeleteScreen: true,
-                      onComplete: () async {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, AppRoutes.prepareUpdate);
-                      },
-                    ),
+        Consumer<WalletProvider>(
+          builder: (context, provider, child) {
+            return ButtonGroup(
+              buttons: [
+                if (provider.vaultList.isNotEmpty)
+                  SingleButton(
+                    title: t.settings_screen.prepare_update,
+                    onPressed: () async {
+                      MyBottomSheet.showBottomSheet_90(
+                        context: context,
+                        child: CustomLoadingOverlay(
+                          child: PinCheckScreen(
+                            pinCheckContext:
+                                PinCheckContextEnum.sensitiveAction,
+                            isDeleteScreen: true,
+                            onComplete: () async {
+                              Navigator.pop(context);
+                              Navigator.pushNamed(
+                                  context, AppRoutes.prepareUpdate);
+                            },
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-            // 임시 메뉴
-            SingleButton(
-              title: '복원하기',
-              onPressed: () async {
-                MyBottomSheet.showBottomSheet_90(
-                  context: context,
-                  child: CustomLoadingOverlay(
-                    child: PinCheckScreen(
-                      pinCheckContext: PinCheckContextEnum.sensitiveAction,
-                      isDeleteScreen: true,
-                      onComplete: () async {
-                        Navigator.pop(context);
-                        Navigator.pushNamed(context, AppRoutes.restorationInfo);
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
+                // 임시 메뉴
+                SingleButton(
+                  title: '복원하기',
+                  onPressed: () async {
+                    MyBottomSheet.showBottomSheet_90(
+                      context: context,
+                      child: CustomLoadingOverlay(
+                        child: PinCheckScreen(
+                          pinCheckContext: PinCheckContextEnum.sensitiveAction,
+                          isDeleteScreen: true,
+                          onComplete: () async {
+                            Navigator.pop(context);
+                            Navigator.pushNamed(
+                                context, AppRoutes.restorationInfo);
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            );
+          },
         ),
+      ],
+    );
+  }
+
+  Widget _advancedUserPart(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
         Container(
-          padding: const EdgeInsets.only(top: 45, bottom: 16),
+          padding: const EdgeInsets.only(bottom: 16),
           child: Text(
             t.settings_screen.advanced_user,
             style: CoconutTypography.body1_16_Bold,
