b989c8581d3a032877e35b0508b302296072b841
ella <eunji@noncelab.com>
2025-08-25 16:25:47 +0900

    style(pin_input): 비밀번호 문자 설정 시 키보드 숨기기 철회

--- lib/screens/common/pin_input_screen.dart
+++ lib/screens/common/pin_input_screen.dart
@@ -109,108 +109,102 @@ class PinInputScreenState extends State<PinInputScreen> {
               isBottom: widget.step == 0,
             )
           : null,
-      body: GestureDetector(
-        behavior: HitTestBehavior.translucent,
-        onTap: () {
-          FocusScope.of(context).unfocus(); // 키보드 내리기
-        },
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(height: widget.initOptionVisible ? 60 : 24),
-              Text(
-                widget.title,
-                style: CoconutTypography.body1_16_Bold,
-                textAlign: TextAlign.center,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(height: widget.initOptionVisible ? 60 : 24),
+            Text(
+              widget.title,
+              style: CoconutTypography.body1_16_Bold,
+              textAlign: TextAlign.center,
+            ),
+            CoconutLayout.spacing_300h,
+            Align(
+              alignment: Alignment.center,
+              child: widget.descriptionTextWidget ?? const Text(''),
+            ),
+            CoconutLayout.spacing_200h,
+            SizedBox(
+              height: 56,
+              child: _pinType == PinType.number ? _buildNumberInput() : _buildCharacterInput(),
+            ),
+            if (widget.canChangePinType && widget.step == 0)
+              Padding(
+                padding: const EdgeInsets.only(top: 12),
+                child: SizedBox(
+                    height: 40,
+                    child: PinTypeToggleButton(
+                        isActive: true, currentPinType: _pinType, onToggle: _togglePinType)),
               ),
-              CoconutLayout.spacing_300h,
-              Align(
-                alignment: Alignment.center,
-                child: widget.descriptionTextWidget ?? const Text(''),
-              ),
-              CoconutLayout.spacing_200h,
-              SizedBox(
-                height: 56,
-                child: _pinType == PinType.number ? _buildNumberInput() : _buildCharacterInput(),
-              ),
-              if (widget.canChangePinType && widget.step == 0)
-                Padding(
-                  padding: const EdgeInsets.only(top: 12),
-                  child: SizedBox(
-                      height: 40,
-                      child: PinTypeToggleButton(
-                          isActive: true, currentPinType: _pinType, onToggle: _togglePinType)),
-                ),
-              CoconutLayout.spacing_200h,
-              Text(
-                widget.errorMessage,
+            CoconutLayout.spacing_200h,
+            Text(
+              widget.errorMessage,
+              style: CoconutTypography.body3_12.setColor(CoconutColors.warningText),
+              textAlign: TextAlign.center,
+            ),
+            Visibility(
+              visible: widget.lastChance,
+              child: Text(
+                widget.lastChanceMessage ?? '',
                 style: CoconutTypography.body3_12.setColor(CoconutColors.warningText),
                 textAlign: TextAlign.center,
               ),
-              Visibility(
-                visible: widget.lastChance,
-                child: Text(
-                  widget.lastChanceMessage ?? '',
-                  style: CoconutTypography.body3_12.setColor(CoconutColors.warningText),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              const SizedBox(height: 40),
-              Expanded(
-                child: IgnorePointer(
-                  ignoring: _pinType == PinType.character,
-                  child: Opacity(
-                    opacity: _pinType == PinType.number ? 1.0 : 0.0,
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: GridView.count(
-                        crossAxisCount: 3,
-                        childAspectRatio: 2,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        children: widget.pinShuffleNumbers.map((key) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: KeyButton(
-                              keyValue: key,
-                              onKeyTap: widget.onKeyTap,
-                              disabled: widget.disabled,
-                            ),
-                          );
-                        }).toList(),
-                      ),
+            ),
+            const SizedBox(height: 40),
+            Expanded(
+              child: IgnorePointer(
+                ignoring: _pinType == PinType.character,
+                child: Opacity(
+                  opacity: _pinType == PinType.number ? 1.0 : 0.0,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: GridView.count(
+                      crossAxisCount: 3,
+                      childAspectRatio: 2,
+                      shrinkWrap: true,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: widget.pinShuffleNumbers.map((key) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: KeyButton(
+                            keyValue: key,
+                            onKeyTap: widget.onKeyTap,
+                            disabled: widget.disabled,
+                          ),
+                        );
+                      }).toList(),
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                  height: widget.initOptionVisible
-                      ? 60
-                      : MediaQuery.sizeOf(context).height <= 640
-                          ? 30
-                          : 100),
-              Visibility(
-                visible: widget.initOptionVisible,
-                replacement: Container(),
-                child: Padding(
-                    padding: EdgeInsets.only(bottom: _characterFocusNode.hasFocus ? 30 : 60),
-                    child: GestureDetector(
-                      onTap: () {
-                        widget.onReset?.call();
-                      },
-                      child: Text(
-                        t.forgot_password,
-                        style: CoconutTypography.body2_14_Bold.setColor(
-                          CoconutColors.black.withOpacity(0.5),
-                        ),
-                        textAlign: TextAlign.center,
+            ),
+            SizedBox(
+                height: widget.initOptionVisible
+                    ? 60
+                    : MediaQuery.sizeOf(context).height <= 640
+                        ? 30
+                        : 100),
+            Visibility(
+              visible: widget.initOptionVisible,
+              replacement: Container(),
+              child: Padding(
+                  padding: EdgeInsets.only(bottom: _characterFocusNode.hasFocus ? 30 : 60),
+                  child: GestureDetector(
+                    onTap: () {
+                      widget.onReset?.call();
+                    },
+                    child: Text(
+                      t.forgot_password,
+                      style: CoconutTypography.body2_14_Bold.setColor(
+                        CoconutColors.black.withOpacity(0.5),
                       ),
-                    )),
-              ),
-            ],
-          ),
+                      textAlign: TextAlign.center,
+                    ),
+                  )),
+            ),
+          ],
         ),
       ),
     );
