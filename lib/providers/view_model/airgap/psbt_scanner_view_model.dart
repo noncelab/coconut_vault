950b46264cbfc73266df616044bf1c76f2ce4e80
ella <eunji@noncelab.com>
2025-09-15 14:27:20 +0900

    fix(서명하기): 서명 가능 멀티시그지갑 찾는 로직 변경
    - 블루월렛 멀티시그지갑 PSBT에는 extendedPublicKeyList가 없고, canSign으로 커버할 수 있는 상황으로 판단하여 일부 로직을 제거

--- lib/providers/view_model/airgap/psbt_scanner_view_model.dart
+++ lib/providers/view_model/airgap/psbt_scanner_view_model.dart
@@ -59,29 +59,18 @@ class PsbtScannerViewModel {
       }
     } else {
       // 멀티시그지갑
-      final psbtMfpSet = parsedPsbt.extendedPublicKeyList
-          .map((extendedKey) => extendedKey.masterFingerprint)
-          .toSet();
       for (final vault in _walletProvider.vaultList) {
         if (vault.vaultType == WalletType.multiSignature) {
-          final multisigVault = vault.coconutVault as MultisignatureVault;
-          final vaultMfpSet =
-              multisigVault.keyStoreList.map((keyStore) => keyStore.masterFingerprint).toSet();
-
-          // PSBT의 모든 MFP가 vault의 MFP set에 포함되어 있는지 확인
-          if (psbtMfpSet.length == vaultMfpSet.length &&
-              psbtMfpSet.every((psbtMfp) => vaultMfpSet.contains(psbtMfp))) {
-            matchingVaultId = vault.id;
-            final canSign = await _walletProvider.getVaultById(matchingVaultId).canSign(psbtBase64);
-            if (!canSign) {
-              Logger.log('❌ 서명 불가능한 지갑 찾음 ${vault.name}');
-              continue;
-            }
-            _signProvider.setVaultListItem(_walletProvider.getVaultById(matchingVaultId));
-            isVaultSigningAllowed = true;
-            Logger.log('✅ 서명 가능한 지갑 찾음 ${vault.name}');
-            break;
+          final canSign = await _walletProvider.getVaultById(vault.id).canSign(psbtBase64);
+          if (!canSign) {
+            Logger.log('❌ 서명 불가능한 지갑 찾음 ${vault.name}');
+            continue;
           }
+          _signProvider.setVaultListItem(_walletProvider.getVaultById(vault.id));
+          isVaultSigningAllowed = true;
+          matchingVaultId = vault.id;
+          Logger.log('✅ 서명 가능한 지갑 찾음 ${vault.name}');
+          break;
         }
       }
     }
