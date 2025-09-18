8ee8c2f1291bd49c35fdbc3c8cca2c8559ef6e09
doey <doey@noncelab.com>
2025-09-18 18:01:15 +0900

    fix(ui): 캐러셀을 없애고 연결 상태와 문구를 보여주는 것으로 수정

--- lib/screens/start_guide/welcome_screen.dart
+++ lib/screens/start_guide/welcome_screen.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:coconut_design_system/coconut_design_system.dart';
 import 'package:coconut_vault/localization/strings.g.dart';
 import 'package:coconut_vault/providers/connectivity_provider.dart';
 import 'package:coconut_vault/providers/visibility_provider.dart';
 import 'package:coconut_vault/widgets/button/fixed_bottom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -19,13 +19,7 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   late ConnectivityProvider _connectivityProvider;
-  late CarouselSliderController _controller;
   late VisibilityProvider _visibilityProvider;
-  int _current = 0;
-  final Set<int> _viewedPages = {
-    0,
-  };
-
   @override
   void initState() {
     super.initState();
@@ -33,8 +27,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     _visibilityProvider = Provider.of<VisibilityProvider>(context, listen: false);
 
     _initConnectionState();
-
-    _controller = CarouselSliderController();
   }
 
   Future<void> _initConnectionState() async {
@@ -57,199 +49,138 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         body: SafeArea(
             child: Stack(
           children: [
-            Center(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CarouselSlider(
-                  carouselController: _controller,
-                  options: CarouselOptions(
-                    autoPlay: false,
-                    aspectRatio: 0.6,
-                    enlargeCenterPage: true,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        _current = index;
-                        _viewedPages.add(index);
-                      });
-                    },
-                  ),
-                  items: [
-                    _buildGuide(1),
-                    _buildGuide(2),
-                    _buildGuide(3),
-                  ].map((item) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 16),
-                      child: item,
-                    );
-                  }).toList(),
-                ),
-                CoconutLayout.spacing_1500h,
-              ],
-            )),
-            Positioned(
-              bottom: 150,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(3, (index) {
-                  return GestureDetector(
-                      onTap: () => _controller.animateToPage(index),
-                      child: Container(
-                        width: _current == index ? 9.0 : 7,
-                        height: _current == index ? 9.0 : 7,
-                        margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: CoconutColors.black.withOpacity(_current == index ? 0.7 : 0.3),
+            Consumer<ConnectivityProvider>(builder: (context, provider, child) {
+              final isNetworkOn = provider.isNetworkOn ?? false;
+              final isBluetoothOn = provider.isBluetoothOn ?? false;
+              final isDeveloperModeOn = provider.isDeveloperModeOn ?? false;
+              final canStart = provider.isNetworkOn == false &&
+                  provider.isBluetoothOn == false &&
+                  (!Platform.isAndroid || provider.isDeveloperModeOn == false);
+
+              return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                        canStart
+                            ? t.welcome_screen.title_can_start
+                            : t.welcome_screen.title_cannot_start,
+                        style: CoconutTypography.heading3_21_Bold,
+                        textAlign: TextAlign.center),
+                    CoconutLayout.spacing_500h,
+                    Align(
+                      alignment: Alignment.center,
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          decoration: BoxDecoration(
+                            color: CoconutColors.gray150,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              _getState(
+                                  t.welcome_screen.network,
+                                  isNetworkOn,
+                                  isNetworkOn
+                                      ? t.connectivity_state.connected
+                                      : t.connectivity_state.disconnected),
+                              CoconutLayout.spacing_300h,
+                              _getState(
+                                  t.welcome_screen.bluetooth,
+                                  isBluetoothOn,
+                                  isBluetoothOn
+                                      ? t.connectivity_state.enabled
+                                      : t.connectivity_state.disabled),
+                              if (Platform.isAndroid) ...[
+                                CoconutLayout.spacing_300h,
+                                _getState(
+                                    t.welcome_screen.developer_option,
+                                    isDeveloperModeOn,
+                                    isDeveloperModeOn
+                                        ? t.connectivity_state.active
+                                        : t.connectivity_state.inactive),
+                              ]
+                            ],
+                          ),
                         ),
-                      ));
-                }),
-              ),
-            ),
+                      ),
+                    ),
+                    CoconutLayout.spacing_2000h,
+                  ]);
+            }),
             Consumer<ConnectivityProvider>(builder: (context, provider, child) {
               final isActive = provider.isNetworkOn == false &&
                   provider.isBluetoothOn == false &&
                   (!Platform.isAndroid || provider.isDeveloperModeOn == false);
 
-              return Visibility(
-                visible: _viewedPages.length == 3,
-                child: FixedBottomButton(
-                  isActive: isActive,
-                  onButtonClicked: () {
-                    _connectivityProvider.setHasSeenGuideTrue();
-                    _visibilityProvider.setHasSeenGuide().then((_) {
-                      widget.onComplete();
-                      if (context.mounted) {
-                        Navigator.pushNamedAndRemoveUntil(
-                          context,
-                          '/',
-                          (Route<dynamic> route) => false,
-                        );
-                      }
-                    });
-                  },
-                  text: t.start,
-                ),
+              return FixedBottomButton(
+                isActive: isActive,
+                onButtonClicked: () {
+                  _connectivityProvider.setHasSeenGuideTrue();
+                  _visibilityProvider.setHasSeenGuide().then((_) {
+                    widget.onComplete();
+                    if (context.mounted) {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/',
+                        (Route<dynamic> route) => false,
+                      );
+                    }
+                  });
+                },
+                text: t.start,
               );
             }),
           ],
         )));
   }
 
-  Widget _buildGuide(
-    int step,
-  ) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(_getMessage(step),
-            style: CoconutTypography.heading3_21_Bold, textAlign: TextAlign.center),
-        CoconutLayout.spacing_1000h,
-        _getImage(step),
-      ],
-    );
-  }
-
-  String _getMessage(int step) {
-    switch (step) {
-      case 1:
-        return t.welcome_screen.guide1;
-      case 2:
-        return t.welcome_screen.guide2;
-      case 3:
-        return t.welcome_screen.guide3;
-    }
-    return '';
-  }
-
-  Widget _getImage(int step) {
-    switch (step) {
-      case 1:
-        return Image.asset('assets/png/onboarding_1.png', height: 160, fit: BoxFit.fitHeight);
-      case 2:
-        return Image.asset('assets/png/onboarding_2.png', height: 160, fit: BoxFit.fitHeight);
-      case 3:
-        return SizedBox(
-          height: Platform.isAndroid ? 170 : 160,
-          child: Consumer<ConnectivityProvider>(builder: (context, provider, child) {
-            final isNetworkOn = provider.isNetworkOn ?? false;
-            final isBluetoothOn = provider.isBluetoothOn ?? false;
-            final isDeveloperModeOn = provider.isDeveloperModeOn ?? false;
-
-            return Align(
-              alignment: Alignment.center,
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 24),
-                decoration: BoxDecoration(
-                  color: CoconutColors.gray150,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    _getState(
-                        t.welcome_screen.network,
-                        isNetworkOn,
-                        isNetworkOn
-                            ? t.connectivity_state.connected
-                            : t.connectivity_state.disconnected),
-                    CoconutLayout.spacing_300h,
-                    _getState(
-                        t.welcome_screen.bluetooth,
-                        isBluetoothOn,
-                        isBluetoothOn
-                            ? t.connectivity_state.enabled
-                            : t.connectivity_state.disabled),
-                    if (Platform.isAndroid) ...[
-                      CoconutLayout.spacing_300h,
-                      _getState(
-                          t.welcome_screen.developer_option,
-                          isDeveloperModeOn,
-                          isDeveloperModeOn
-                              ? t.connectivity_state.active
-                              : t.connectivity_state.inactive),
-                    ]
-                  ],
-                ),
-              ),
-            );
-          }),
-        );
-    }
-    return Container();
-  }
-
   Widget _getState(String label, bool isOn, String stateText) {
     return Row(
       children: [
         CoconutLayout.spacing_600w,
-        Expanded(flex: 5, child: Text(label, style: CoconutTypography.body1_16_Bold, maxLines: 2)),
+        Text(label,
+            style: isOn
+                ? CoconutTypography.body1_16_Bold.setColor(CoconutColors.hotPink)
+                : CoconutTypography.body1_16_Bold.setColor(CoconutColors.black),
+            maxLines: 2),
+        if (isOn) ...[
+          CoconutLayout.spacing_100w,
+          SvgPicture.asset(
+            'assets/svg/triangle-warning.svg',
+            width: 16.0,
+            height: 16.0,
+            colorFilter: const ColorFilter.mode(
+              CoconutColors.hotPink,
+              BlendMode.srcIn,
+            ),
+          ),
+        ],
         CoconutLayout.spacing_100w,
         Expanded(
-            flex: 4,
-            child: Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                decoration: BoxDecoration(
-                  color: isOn
-                      ? const Color.fromARGB(255, 236, 39, 35)
-                      : const Color.fromARGB(255, 105, 224, 119),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Text(
-                    textAlign: TextAlign.center,
-                    stateText,
-                    style: CoconutTypography.body3_12_Bold.setColor(CoconutColors.white)),
+          flex: 4,
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: isOn
+                    ? const Color.fromARGB(255, 236, 39, 35)
+                    : const Color.fromARGB(255, 95, 211, 109),
+                borderRadius: BorderRadius.circular(20),
               ),
-            )),
+              child: Text(
+                  textAlign: TextAlign.center,
+                  stateText,
+                  style: CoconutTypography.body3_12_Bold.setColor(CoconutColors.white)),
+            ),
+          ),
+        ),
         CoconutLayout.spacing_600w,
       ],
     );
