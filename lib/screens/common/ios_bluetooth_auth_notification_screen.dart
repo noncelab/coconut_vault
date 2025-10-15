36b575c1b273342fd2a44d9ab05ffde398f8958e
doey <doey@noncelab.com>
2025-10-15 21:31:21 +0900

    fix(ui): ios 권한 안내 화면 수정, welcome_screen의 설정 가이드 내용 보완

--- lib/screens/common/ios_bluetooth_auth_notification_screen.dart
+++ lib/screens/common/ios_bluetooth_auth_notification_screen.dart
@@ -1,7 +1,10 @@
 import 'package:coconut_design_system/coconut_design_system.dart';
 import 'package:coconut_vault/localization/strings.g.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:lottie/lottie.dart';
 
+/// iOS 블루투스 권한 허용 알림 화면
 class IosBluetoothAuthNotificationScreen extends StatefulWidget {
   const IosBluetoothAuthNotificationScreen({super.key});
 
@@ -13,33 +16,52 @@ class _IosBluetoothAuthNotificationScreenState extends State<IosBluetoothAuthNot
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          color: CoconutColors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                t.ios_bluetooth_auth_notification_screen.allow_permission,
-                style: CoconutTypography.heading3_21_Bold,
-                textAlign: TextAlign.center,
-              ),
-              CoconutLayout.spacing_800h,
-              Image.asset('assets/png/state/bluetooth_on.png', width: 140, fit: BoxFit.fitWidth),
-              CoconutLayout.spacing_800h,
-              Column(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: CoconutColors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              t.ios_bluetooth_auth_notification_screen.allow_permission,
+              style: CoconutTypography.heading3_21_Bold,
+              textAlign: TextAlign.center,
+            ),
+            CoconutLayout.spacing_800h,
+            // 블루투스 토글 로티
+            Container(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: 100,
+              decoration: BoxDecoration(color: const Color(0xFFF7F7F7), borderRadius: BorderRadius.circular(16)),
+              child: Stack(
                 children: [
-                  _buildStep('1', t.ios_bluetooth_auth_notification_screen.guide1),
-                  CoconutLayout.spacing_400h,
-                  _buildStep('2', t.ios_bluetooth_auth_notification_screen.guide2),
-                  CoconutLayout.spacing_400h,
-                  _buildStep('3', t.ios_bluetooth_auth_notification_screen.guide3),
+                  Positioned(
+                    left: 26,
+                    top: 26,
+                    child: SvgPicture.asset('assets/svg/bluetooth.svg', width: 48, height: 48),
+                  ),
+                  Positioned(
+                    right: -5,
+                    top: -25,
+                    child: SizedBox(
+                      height: 150,
+                      width: 150,
+                      child: Lottie.asset('assets/lottie/toggle-switch.json', repeat: true, animate: true),
+                    ),
+                  ),
                 ],
               ),
-              const SizedBox(height: 200),
-            ],
-          ),
+            ),
+            CoconutLayout.spacing_800h,
+            Column(
+              children: [
+                _buildStep('1', t.ios_bluetooth_auth_notification_screen.guide1),
+                CoconutLayout.spacing_400h,
+                _buildStep('2', t.ios_bluetooth_auth_notification_screen.guide2),
+              ],
+            ),
+            // const SizedBox(height: 200),
+          ],
         ),
       ),
     );
