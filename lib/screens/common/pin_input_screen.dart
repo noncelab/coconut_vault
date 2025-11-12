236cb1e28337fd8c91f8ea738976f62911776db4
roy-noncelab <roy@noncelab.com>
2025-11-12 16:02:30 +0900

    feat(pin-check): 생체인증 버튼 배경색상 재수정

--- lib/screens/common/pin_input_screen.dart
+++ lib/screens/common/pin_input_screen.dart
@@ -139,11 +139,11 @@ class PinInputScreenState extends State<PinInputScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
     final keyboardBackgroundColor =
         Platform.isIOS
-            ? (isDarkMode ? const Color(0x00000082) : const Color(0xFFCED2D9))
-            : (isDarkMode ? CoconutColors.gray400 : CoconutColors.gray150);
+            ? (isDarkMode ? CoconutColors.gray750 : CoconutColors.gray300)
+            : (isDarkMode ? CoconutColors.gray900 : CoconutColors.whiteLilac);
 
     final isBiometricEnabled = Provider.of<AuthProvider>(context, listen: false).isBiometricEnabled;
 
