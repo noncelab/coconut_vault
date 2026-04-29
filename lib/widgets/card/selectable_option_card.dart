05eedfc5f8707f10b480042a7af7b9d036b59f67
roy-noncelab <roy@noncelab.com>
2026-04-29 10:57:19 +0900

    feat(selectable_option_card): 위젯 구현

--- lib/widgets/card/selectable_option_card.dart
+++ lib/widgets/card/selectable_option_card.dart
@@ -1,1 +1,67 @@
-class SelectableOptionCard {}
+import 'package:coconut_design_system/coconut_design_system.dart';
+import 'package:coconut_vault/widgets/fixed_text_scale.dart';
+import 'package:flutter/material.dart';
+
+class SelectableOptionCard extends StatelessWidget {
+  final String title;
+  final String? description;
+  final String svgAssetPath;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final double width;
+  final double height;
+
+  const SelectableOptionCard({
+    super.key,
+    required this.title,
+    this.description,
+    required this.svgAssetPath,
+    required this.isSelected,
+    required this.onTap,
+    this.width = double.infinity,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: isSelected ? CoconutColors.gray150 : CoconutColors.white,
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(
+            color: isSelected ? CoconutColors.gray800 : CoconutColors.gray200,
+            width: isSelected ? 1.0 : 1.0,
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.topLeft,
+          children: [
+            Positioned(
+              top: 20,
+              left: 20,
+              right: 13,
+              child: FixedTextScale(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(title, style: CoconutTypography.body1_16_Bold.setColor(CoconutColors.black)),
+                    if (description != null) ...[
+                      const SizedBox(height: 2),
+                      Text(description!, style: CoconutTypography.body3_12.setColor(CoconutColors.black)),
+                    ],
+                  ],
+                ),
+              ),
+            ),
+            Positioned(bottom: 12, right: 12, child: Image.asset(svgAssetPath)),
+          ],
+        ),
+      ),
+    );
+  }
+}
