9f1c2cbec4c6c421184c1d11ad54594bdc588fe0
pio <pio@noncelab.com>
2026-05-07 18:56:43 +0900

    chore(taproot-setup-summary-card): 불필요 import 제거

--- lib/widgets/card/taproot/taproot_setup_summary_card.dart
+++ lib/widgets/card/taproot/taproot_setup_summary_card.dart
@@ -2,7 +2,6 @@ import 'package:coconut_design_system/coconut_design_system.dart';
 import 'package:coconut_vault/localization/strings.g.dart';
 import 'package:coconut_vault/widgets/card/taproot/taproot_participant_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class TaprootSetupSummaryCard extends StatelessWidget {
   static const double _guideLineX = 0;
